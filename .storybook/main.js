b7e0096e9f230a37eb2dc88058e0574252e77ce3
with-heart <with.heart+git@pm.me>
2022-10-15 02:48:07 -0400

    start the fuck over

--- .storybook/main.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-  stories: ['../**/*.stories.mdx', '../**/*.stories.@(js|jsx|ts|tsx)'],
-  addons: ['@storybook/addon-links', '@storybook/addon-essentials'],
-  framework: '@storybook/react',
-  core: {
-    builder: 'webpack5',
-  },
-  typescript: {reactDocgen: false},
-}
