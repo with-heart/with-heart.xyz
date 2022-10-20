124e03c7bdac73a18ab0bd5bbf8c186cbfc831aa
with-heart <with.heart+git@pm.me>
2022-10-20 03:21:19 -0400

    add i18n config to next.config

--- site/next.config.mjs
+++ site/next.config.mjs
@@ -5,6 +5,10 @@ const nextConfig = {
   pageExtensions: ['ts', 'tsx', 'js', 'jsx', 'md', 'mdx'],
   reactStrictMode: true,
   swcMinify: true,
+  i18n: {
+    locales: ['en'],
+    defaultLocale: 'en',
+  },
 }
 
 export default withMdx(nextConfig)
