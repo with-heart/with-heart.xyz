df3fdbc78b66a408eaa85fe042da72a161c2fbc6
with-heart <with.heart+git@pm.me>
2022-10-19 22:19:21 -0400

    add a little responsiveness to page

--- site/src/layout.tsx
+++ site/src/layout.tsx
@@ -1,13 +1,19 @@
 import Link from 'next/link'
+import {useRouter} from 'next/router'
 import {ReactNode} from 'react'
 import styles from './layout.module.scss'
 
 export const Layout = ({children}: {children: ReactNode}) => {
+  const {route} = useRouter()
+  const isIndex = route === '/'
+
   return (
     <div className={styles.layout}>
-      <header>
-        <Link href="/">🌱❤️‍🔥</Link>
-      </header>
+      {isIndex ? null : (
+        <header>
+          <Link href="/">🌱❤️‍🔥</Link>
+        </header>
+      )}
       <>{children}</>
     </div>
   )
--- site/src/pages/_app.tsx
+++ site/src/pages/_app.tsx
@@ -1,16 +1,22 @@
 import {MDXProvider} from '@mdx-js/react'
 import type {AppProps} from 'next/app'
+import Head from 'next/head'
 import {components} from '../components'
 import {Layout} from '../layout'
 import '../styles/global.scss'
 
 function MyApp({Component, pageProps}: AppProps) {
   return (
-    <Layout>
-      <MDXProvider components={components}>
-        <Component {...pageProps} />
-      </MDXProvider>
-    </Layout>
+    <>
+      <Head>
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+      </Head>
+      <Layout>
+        <MDXProvider components={components}>
+          <Component {...pageProps} />
+        </MDXProvider>
+      </Layout>
+    </>
   )
 }
 
