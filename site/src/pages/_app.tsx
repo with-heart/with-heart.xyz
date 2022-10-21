5199f04df756d27546c6338fc3462ba0d2ef1570
with-heart <with.heart+git@pm.me>
2022-10-21 03:32:54 -0400

    add twitter metadata

--- site/src/pages/_app.tsx
+++ site/src/pages/_app.tsx
@@ -34,6 +34,11 @@ function MyApp({
         <link rel="manifest" href="/site.webmanifest" />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <meta name="description" content={pageProps.description} />
+        <meta name="twitter:card" content="summary" />
+        <meta name="twitter:site" content="@grow_love" />
+        <meta name="twitter:creator" content="@grow_love" />
+        <meta name="twitter:title" content={title} />
+        <meta name="twitter:description" content={pageProps.description} />
       </Head>
       <Layout>
         <MDXProvider components={components}>
