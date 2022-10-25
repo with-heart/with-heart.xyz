6957eb58c7fe72a5f332e25ceba1eede1105a224
with-heart <with.heart+git@pm.me>
2022-10-25 12:19:51 -0400

    add components example to @vercel/og page

--- site/src/pages/api/img/basic.tsx
+++ site/src/pages/api/img/basic.tsx
@@ -1,5 +1,6 @@
 import {ImageResponse} from '@vercel/og'
 import {NextRequest} from 'next/server'
+import {ReactNode} from 'react'
 
 export const config = {
   runtime: 'experimental-edge',
@@ -40,6 +41,45 @@ export default function BasicImg(req: NextRequest) {
           height: 200,
         },
       )
+    case 4: {
+      const Container = ({children}: {children: ReactNode}) => (
+        <div
+          style={{
+            width: '100%',
+            height: '100%',
+            display: 'flex',
+            alignItems: 'center',
+            justifyContent: 'center',
+            backgroundColor: 'white',
+            border: '1px solid black',
+          }}
+        >
+          {children}
+        </div>
+      )
+
+      const Text = ({children}: {children: ReactNode}) => (
+        <div
+          style={{
+            fontSize: 64,
+          }}
+        >
+          {children}
+        </div>
+      )
+
+      return new ImageResponse(
+        (
+          <Container>
+            <Text>Hello World</Text>
+          </Container>
+        ),
+        {
+          width: 400,
+          height: 200,
+        },
+      )
+    }
     case 1:
     default:
       return new ImageResponse(<div>Hello World</div>)
